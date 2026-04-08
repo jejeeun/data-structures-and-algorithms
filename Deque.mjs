82fcfc7ae32363cb16ff1b85a1b1f3780439e097
jejeeun <68427684+jejeeun@users.noreply.github.com>
2026-04-08 14:56:30 +0900

    Add deque implementation and tests

---
+++ Deque.mjs
@@ -0,0 +1,38 @@
+import { DoublyLinkedList } from "./DoublyLinkedList.mjs";
+
+class Deque{
+    constructor(){
+        this.list = new DoublyLinkedList();
+    }
+
+    printAll(){
+        this.list.printAll();
+    }
+
+    addFirst(data){
+        this.list.insertAt(0, data)
+    }
+
+    removeFirst(){
+        return this.list.deleteAt(0)
+    }
+
+    addLast(data){
+        this.list.insertAt(this.list.count, data)
+    }
+
+
+    removeLast(){
+        try{
+            return this.list.deleteLast()
+        }catch(e){
+            return null
+        }
+    }
+
+    isEmpty(){
+        return this.list.count == 0
+    }
+}
+
+export { Deque }
